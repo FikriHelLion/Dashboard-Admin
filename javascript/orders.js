d349189b2f84d197dbfe734ca7ea7a9ea4a2b70f
unknown <fikrihellion@gmail.com>
2024-09-19 13:08:35 +0700

    dashboard admin

---
+++ javascript/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Polo Lacoste',
+        productNumber: '85743',
+        paymentStatus: 'COD',
+        status: 'Pending'
+    },
+    {
+        productName: 'Jacket Bomber Army',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Sepatu Adidas Samba',
+        productNumber: '36452',
+        paymentStatus: 'COD',
+        status: 'Active'
+    },
+]
